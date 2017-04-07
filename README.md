9fb84bd5890069f4b20a4d885a6d4915f9b0197d
Hendrik Bunke <h.bunke@zbw.eu>
2017-04-07 08:55:28 +0200

    build vimEnv with nix in vim dir
    (not with ~/.nixpkgs/config.nix)

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
 Vim Configuration
 =================
 
-Most Vim plugins here are installed with vimEnv in `~/.nixpkgs`. Packages are
-mirrored in default.nix to test or develop in nix-shell. Non-nix packages can
+Most Vim plugins here are installed with `vimEnv` in `vimEnv.nix`. Non-nix packages can
 be managed in vim-native `pack/` (since Vim 8) using `git submodule`. After
 cloning execute `git submodule init` and `git submodule update`. To install
 python-jedi go to `pack/hbunke/start/jedi-vim` and do `git submodule update
---init` (jedi is an git submodule within the jedi repository). 
+--init` (jedi is an git submodule within the jedi repository).
+
+## Steps to install
+
+-   build and install the nix env:
+    `nix-env -f ./vimEnv.nix -i vimEnv`
+
+-   clone and activate the git submodules in `pack/hbunke/start`:
+    `git submodule update --init`
+
+-   clone and activate python jedi rep:
+    `cd pack/hbunke/start/jedi-vim && git submodule update --init`
---
+++ vimEnv.nix
@@ -0,0 +1,41 @@
+# nix env for Vim. Most of used plugins are installed here, only those not
+# available in nixpkgs are managed in ./pack/hbunke/start/ (Vim 8!) as git
+# submodules
+
+# install with nix-env -f vimEnv.nix -i vimEnv
+
+with import <nixpkgs> {};
+buildEnv {
+  name = "vimEnv";
+  paths = [ 
+      # UPDATE: flake8 seems to work when installing it in pack/... (vim8)
+      # UPDATE: you can install jedi inside the jedi-vim package with:
+      # 'git submodule update --init'
+      # see jedi-vim/README.rst
+        vim_configurable
+        python27Full
+        ctags
+        ack
+        ag
+        python27Packages.flake8
+        vimPlugins.ctrlp
+        vimPlugins.nerdtree
+        vimPlugins.tagbar
+        vimPlugins.taglist
+        vimPlugins.Syntastic
+        vimPlugins.pluginnames2nix
+        vimPlugins.vim-jinja
+        vimPlugins.vim-nerdtree-tabs
+        vimPlugins.vim-buffergator
+        vimPlugins.fugitive
+        vimPlugins.vim-scala
+        vimPlugins.airline
+        vimPlugins.vim-colorschemes
+        vimPlugins.vim-airline-themes
+        vimPlugins.The_NERD_Commenter
+        vimPlugins.gruvbox  
+        vimPlugins.vim-indent-object
+
+      ];
+}
+
